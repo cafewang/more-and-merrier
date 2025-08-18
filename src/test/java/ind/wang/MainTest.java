70a25264ff09e852d10c41a62ea01d9fc2be2c58
cafewang <434637878@qq.com>
2025-08-19 00:02:15 +0800

    add worker in shutdown state

--- src/test/java/ind/wang/MainTest.java
+++ src/test/java/ind/wang/MainTest.java
@@ -20,7 +20,7 @@ class MainTest {
     }
 
     @Test
-    void addWorkerReturnFalseInStopState() throws Exception {
+    void addWorkerReturnFalseInStopState() {
         InvocationRecorder.reset();
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         CountDownLatch signal = new CountDownLatch(1);
@@ -38,6 +38,34 @@ class MainTest {
         executorService.submit(waitUntilSignal);
         executorService.shutdownNow();
         Assertions.assertEquals(ThreadPoolInspector.STOP, ThreadPoolInspector.getState((ThreadPoolExecutor) executorService));
+        try {
+            executorService.submit(() -> System.out.println("task rejected"));
+        } catch (Exception ignored) {
+        }
+        Assertions.assertEquals(3, InvocationRecorder.getInvocations());
+        Assertions.assertEquals(false, InvocationRecorder.getReturnValue(1));
+        Assertions.assertEquals(false, InvocationRecorder.getReturnValue(2));
+    }
+
+    @Test
+    void addWorkerReturnFalseInShutdownState() {
+        InvocationRecorder.reset();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        CountDownLatch signal = new CountDownLatch(1);
+        Runnable waitUntilSignal = () -> {
+            try {
+                signal.await();
+            } catch (InterruptedException e) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        };
+        executorService.submit(waitUntilSignal);
+        executorService.shutdown();
+        Assertions.assertEquals(ThreadPoolInspector.SHUTDOWN, ThreadPoolInspector.getState((ThreadPoolExecutor) executorService));
         try {
             executorService.submit(() -> System.out.println("task rejected"));
         } catch (Exception ignored) {
